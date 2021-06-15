e98eb6765188f9da73ac3c8395e336156553ff7a
Boya <vby@amazon.com>
2021-06-15 20:25:39 +0200

    testing day before0

--- hello-world.cob
+++ hello-world.cob
@@ -5,8 +5,6 @@
        WORKING-STORAGE SECTION.
        01 HELLO-X            PIC X(40).
        PROCEDURE DIVISION.
-       DISPLAY "Hello Everyone".
-       DISPLAY "Thanks for coming to this session".
        DISPLAY "How to modernize your legacy COBOL applications".
        STOP RUN.
        
