25845f919f5ebcfcb02cfb34c402c3b190a3d01e
Boya <vby@amazon.com>
2021-06-15 15:49:32 +0200

    testing day before

--- hello-world.cob
+++ hello-world.cob
@@ -1,6 +1,16 @@
        IDENTIFICATION DIVISION.
-       PROGRAM-ID. hello.
+       PROGRAM-ID. OpenAtAmazon.
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 HELLO-X.
+          05 HELLO-1             PIC X(15).
+          05 HELLO-2             PIC X(25).
+          05 HELLO-3             PIC X(60).
        PROCEDURE DIVISION.
-       DISPLAY "Hello World from COBOL!".
+       MOVE "Hello Everyone" TO HELLO-1.
+       MOVE "Welcome to OpenAtAmazon" to HELLO-2.
+       MOVE "I hope your enjoyed this lambda-cobol OpenSource Demo" TO HELLO-3.
+       WRITE HELLO-X.
        STOP RUN.
        
