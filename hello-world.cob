8309d0ad0a47ef2d3c9cf5eaa76f4e14fd97e55c
Boya <vby@amazon.com>
2021-06-16 16:07:50 +0200

    testing day beforeH

--- hello-world.cob
+++ hello-world.cob
@@ -5,6 +5,6 @@
        WORKING-STORAGE SECTION.
        01 HELLO-X            PIC X(40).
        PROCEDURE DIVISION.
-       DISPLAY "Hello OpenATAmazon".
+       DISPLAY "Hello OpenAtAmazon".
        STOP RUN.
        
